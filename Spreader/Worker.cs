f2a50510ed0875bcd9475a64e2f6aaaafa8b891b
Luke Croteau <luke.j.croteau@gmail.com>
2019-05-22 12:34:09 -0400

    Fix Initialization Typo

--- Spreader/Worker.cs
+++ Spreader/Worker.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using System.Diagnostics;
@@ -260,7 +260,7 @@ namespace Spreader
 
             ClientInitialized = true;
 
-            SendToSocket("WKRINITIALIED");
+            SendToSocket("WKRINITIALIZED");
         }
 
         private void HandleClientPing()
